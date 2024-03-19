714380307d447fe17dc857a83b45048e4cdca020
Gilbert <gilbertbgarza@gmail.com>
2024-03-18 19:21:11 -0500

    Reproduce bug

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import { LitAuthClient } from "@lit-protocol/lit-auth-client";
+
+console.log('LitAuthClient', LitAuthClient)
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+  </div>
+`
